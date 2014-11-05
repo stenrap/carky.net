b2c8e33354e318304c02366c2d72a9bfe6a61753
Rob Johansen <robert_johans22@hotmail.com>
2014-11-04 22:28:53 -0700

    Get Hibernate configured.

--- src/main/java/net/carky/config/RootConfig.java
+++ src/main/java/net/carky/config/RootConfig.java
@@ -1,16 +1,22 @@
 package net.carky.config;
 
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
+@EnableTransactionManagement
 public class RootConfig {
 
     @Bean
@@ -23,6 +29,21 @@ public class RootConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() throws NamingException, IOException {
+        LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
+        lsfb.setDataSource(dataSource());
+        lsfb.setPackagesToScan(new String[] {"net.carky.model"});
+        lsfb.afterPropertiesSet();
+        return lsfb.getObject();
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() throws NamingException, IOException {
+        HibernateTransactionManager htm = new HibernateTransactionManager(sessionFactory());
+        return htm;
     }
 
 }
